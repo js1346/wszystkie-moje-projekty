582eb816a14b4f6959f6331bc5ddd890d47b388e
js1346 <jakuskalanego@gmail.com>
2026-02-27 15:35:47 +0100

    Add files via upload

---
+++ #include iostream.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{ 
+    int n; cin>>n;
+    char p;
+    vector <int> a(n);
+    int i=0;
+    while(i<n)
+    {
+        cin>>p;
+        if(p=='O') a[i]++;
+        else i++;
+    }
+    sort(a.begin(),a.end());
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<a[i];j++) cout<<"O";
+        cout<<endl;
+    }
+
+    return 0;
+}
