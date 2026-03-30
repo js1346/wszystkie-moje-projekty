2cdf428cb040df8809ae9a8c590784959a51d2b6
js1346 <jakuskalanego@gmail.com>
2026-03-30 18:10:48 +0200

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -1,21 +1,32 @@
+package ex4;
 import java.util.Scanner;
+
 
 public class Ex4 {
     public static void main(String[] args) {
 
         Scanner scanner=new Scanner(System.in);
-        scanner.useDelimiter(" ");
-        scanner.useDelimiter("\n" + "");
+
         int n;
         int m;
+        String t1emp=new String();
+        String t2emp=new String();
 
         n=scanner.nextInt();
         m=scanner.nextInt();
-        System.out.print(n + " " + m);
+        Graph graph=new Graph(n);
 
-        for(int i=0 ; i<n; i++){
-            System.out.print(n + " " + m);
+
+        for(int i=0 ; i<m; i++){
+            t1emp=scanner.next();
+            t2emp=scanner.next();
+            graph.addVertex(t1emp);
+            graph.addVertex(t2emp);
+            graph.addEdge(t1emp,t2emp);
         }
+
+        String toReturn=graph.findTriangle();
+        System.out.print(toReturn);
 
     }
 }
